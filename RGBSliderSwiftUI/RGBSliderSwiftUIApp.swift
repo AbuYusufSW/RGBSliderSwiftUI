4ffbe695aa5192c7c82a4430477e282798c24b79
AbuYusuf <AbuYusufSW@gmail.com>
2023-06-13 16:38:15 +0300

    Initial Commit

---
+++ RGBSliderSwiftUI/RGBSliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBSliderSwiftUIApp.swift
+//  RGBSliderSwiftUI
+//
+//  Created by AbuYusuf on 13.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct RGBSliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
